bf2c75219987648826d55a41e4d22712ba95f0b8
Marcelo Peñafiel <penafieldavid471@gmail.com>
2023-12-29 02:10:02 -0500

    Floating window error config

--- init.lua
+++ init.lua
@@ -4,3 +4,13 @@ vim.wo.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
+
